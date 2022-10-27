ddf7d9b9c40dd8ae0ebba275970815e5ba2e8bda
Erik Nijenhuis <erik@xerdi.com>
2022-10-27 17:21:08 +0200

    Add full calendar plugin

--- admin-lte.js
+++ admin-lte.js
@@ -1,8 +1,10 @@
 // CSS Dependencies
+import 'admin-lte/plugins/fontawesome-free/css/all.min.css'
 import 'admin-lte/plugins/select2/css/select2.min.css';
 import 'admin-lte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css';
 import 'admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css';
 import 'admin-lte/plugins/bs-stepper/css/bs-stepper.min.css';
+import 'admin-lte/plugins/fullcalendar/main.css';
 import 'admin-lte/dist/css/adminlte.min.css';
 
 // JavaScript Dependencies
@@ -12,7 +14,8 @@ require('admin-lte/plugins/bootstrap-switch/js/bootstrap-switch.min');
 require('admin-lte/plugins/bs-stepper/js/bs-stepper.min');
 const select2 = require('admin-lte/plugins/select2/js/select2.min');
 require('@fortawesome/fontawesome-free/js/all.min');
-require('admin-lte/dist/js/adminlte.min');
+export const AdminLTE = require('admin-lte/dist/js/adminlte.min');
+require('admin-lte/plugins/moment/moment.min');
 
 select2(window, jQuery);
 
